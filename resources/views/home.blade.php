00fba455cbfbb835b8ce3ff07250ab171e1fe100
Thomas Simbamwene <bs.thomas@hotmail.com>
2024-05-27 01:02:28 +0300

    v1.0

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<!-- ========== MAIN CONTENT ========== -->
+      <div class="text-center py-10 px-4 sm:px-6 lg:px-8">
+        <h1 class="block text-7xl font-bold text-[#FFFFFF] sm:text-9xl font-montserrat">Create Something New!</h1>
+        <p class="mt-3 text-[#D3D8E8] font-poppins">How Easy this is!</p>
+        <p class="text-[#D3D8E8] font-poppins">With all added Styles ready to Use.</p>
+        <div class="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+          <a class="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none font-lato" href="https://digitalgearjunction.africa">
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+            Visit Our Website
+          </a>
+        </div>
+      </div>
+
+    <!-- ========== END MAIN CONTENT ========== -->
+@endsection
